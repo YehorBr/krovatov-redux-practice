40f23eccdf9ae115ce818a845a798e1fbf516d39
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2025-09-12 20:55:03 +0300

    .

--- src/Pages/Catalog/Catalog.jsx
+++ src/Pages/Catalog/Catalog.jsx
@@ -1,3 +1,35 @@
+
+
+import { useState } from "react";
 export const Catalog = () => {
-  <h2>Catalog</h2>;
+  <h2>Каталог</h2>;
 };
+
+
+
+export const CatalogMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const categories = [
+    { name: "Ліжка", icon: "" },
+    { name: "Матраци", icon: "" },
+    { name: "М’які меблі", icon: "" },
+    { name: "Шафи", icon: "" },
+    { name: "Комоди", icon: "" },
+    { name: "Тумби", icon: "" },
+    { name: "Столи", icon: "" },
+    { name: "Стільці", icon: "" },
+    { name: "Меблеві стіни", icon: "" },
+    { name: "Кухні", icon: "" },
+  ];
+  return(
+  <div >
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        Каталог
+      </button>
+      </div>
+  )
+      }
+       
