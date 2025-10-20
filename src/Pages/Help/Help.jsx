e5b2e0e17f54f4516c07cc9d406a8ccf04745f01
AddNiI <100078200+AddNiI@users.noreply.github.com>
2025-10-20 20:22:58 +0300

    c

--- src/Pages/Help/Help.jsx
+++ src/Pages/Help/Help.jsx
@@ -6,18 +6,20 @@ import './style.css'
 export const Help = () => {
   return (
     <>
+    <div className='Help-block'>
       <p className='Help'>Допомога і консультація:</p>
-      <ul>
-        <li>
+      <ul className='Help-icon-block'>
+        <li className="bali">
           <FaWhatsapp className='Help-icon-wa' />
         </li>
-        <li>
+        <li className="bali">
           <FaTelegram className='Help-icon-tg' />
         </li>
-        <li>
+        <li className="bali">
           <FaViber className='Help-icon-vb' />
         </li>
       </ul>
+      </div>
     </>
   );
 };
